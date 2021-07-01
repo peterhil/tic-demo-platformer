d02d38e7b489c005f48c6ca5d6257ba5611ffff3
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-07-02 02:42:27 +0300

    Add Demo platformer code, map and sprites

--- src/main.lua
+++ src/main.lua
@@ -1,21 +1,50 @@
--- title:  game title
--- author: game developer
--- desc:   short description
+-- title:  Platformer Demo
+-- author: trelemar / pkh
+-- desc:   Demo made for tutorial @ infinitelimit.net
 -- script: lua
+-- input:  gamepad
 
-t=0
-x=96
-y=24
+function solid(x,y)
+    return solids[mget((x)//8,(y)//8)]
+end
 
+function init()
+    solids={[1]=true,[3]=true}
+    p={
+    x=120,
+    y=68,
+    vx=0, --Velocity X
+    vy=0, --Velocity Y
+}
+end
+
+init()
 function TIC()
+    if btn(2) then p.vx=-1
+        elseif btn(3) then p.vx=1
+        else p.vx=0
+    end
 
-	if btn(0) then y=y-1 end
-	if btn(1) then y=y+1 end
-	if btn(2) then x=x-1 end
-	if btn(3) then x=x+1 end
+    if solid(p.x+p.vx,p.y+p.vy) or solid(p.x+7+p.vx,p.y+p.vy) or solid(p.x+p.vx,p.y+7+p.vy) or solid(p.x+7+p.vx,p.y+7+p.vy) then
+        p.vx=0
+    end
 
-	cls(13)
-	spr(1+t%60//30*2,x,y,14,3,0,0,2,2)
-	print("HELLO WORLD!",84,84)
-	t=t+1
+    if solid(p.x,p.y+8+p.vy) or solid(p.x+7,p.y+8+p.vy) then
+        p.vy=0
+    else
+        p.vy=p.vy+0.2
+    end
+
+    if p.vy==0 and btnp(4) then p.vy=-2.5 end
+
+    if p.vy<0 and (solid(p.x+p.vx,p.y+p.vy) or solid(p.x+7+p.vx,p.y+p.vy)) then
+        p.vy=0
+    end
+
+    p.x=p.x+p.vx
+    p.y=p.y+p.vy
+
+    cls()
+    map()
+    rect(p.x,p.y,8,8,15)
 end
