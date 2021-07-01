c453ddaaf3f6391fb79a8080c0d584a983177584
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-07-02 02:44:35 +0300

    Add .editorconfig and use tabs

--- src/main.lua
+++ src/main.lua
@@ -5,46 +5,46 @@
 -- input:  gamepad
 
 function solid(x,y)
-    return solids[mget((x)//8,(y)//8)]
+	return solids[mget((x)//8,(y)//8)]
 end
 
 function init()
-    solids={[1]=true,[3]=true}
-    p={
-    x=120,
-    y=68,
-    vx=0, --Velocity X
-    vy=0, --Velocity Y
-}
+	solids={[1]=true,[3]=true}
+	p={
+		x=120,
+		y=68,
+		vx=0, --Velocity X
+		vy=0, --Velocity Y
+	}
 end
 
 init()
 function TIC()
-    if btn(2) then p.vx=-1
-        elseif btn(3) then p.vx=1
-        else p.vx=0
-    end
+	if btn(2) then p.vx=-1
+	elseif btn(3) then p.vx=1
+	else p.vx=0
+	end
 
-    if solid(p.x+p.vx,p.y+p.vy) or solid(p.x+7+p.vx,p.y+p.vy) or solid(p.x+p.vx,p.y+7+p.vy) or solid(p.x+7+p.vx,p.y+7+p.vy) then
-        p.vx=0
-    end
+	if solid(p.x+p.vx,p.y+p.vy) or solid(p.x+7+p.vx,p.y+p.vy) or solid(p.x+p.vx,p.y+7+p.vy) or solid(p.x+7+p.vx,p.y+7+p.vy) then
+		p.vx=0
+	end
 
-    if solid(p.x,p.y+8+p.vy) or solid(p.x+7,p.y+8+p.vy) then
-        p.vy=0
-    else
-        p.vy=p.vy+0.2
-    end
+	if solid(p.x,p.y+8+p.vy) or solid(p.x+7,p.y+8+p.vy) then
+		p.vy=0
+	else
+		p.vy=p.vy+0.2
+	end
 
-    if p.vy==0 and btnp(4) then p.vy=-2.5 end
+	if p.vy==0 and btnp(4) then p.vy=-2.5 end
 
-    if p.vy<0 and (solid(p.x+p.vx,p.y+p.vy) or solid(p.x+7+p.vx,p.y+p.vy)) then
-        p.vy=0
-    end
+	if p.vy<0 and (solid(p.x+p.vx,p.y+p.vy) or solid(p.x+7+p.vx,p.y+p.vy)) then
+		p.vy=0
+	end
 
-    p.x=p.x+p.vx
-    p.y=p.y+p.vy
+	p.x=p.x+p.vx
+	p.y=p.y+p.vy
 
-    cls()
-    map()
-    rect(p.x,p.y,8,8,15)
+	cls()
+	map()
+	rect(p.x,p.y,8,8,15)
 end
